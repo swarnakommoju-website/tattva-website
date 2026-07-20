cd7b30c239c209a5490a692edcf514512a7abb7e
sneha6129 <krothapallisneha2003@gmail.com>
2026-07-20 17:15:42 +0530

    final commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Services } from './pages/services/services';
+import { AboutUs } from './pages/about-us/about-us';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'services',
     component: Services
+  },
+  { 
+    path: 'about-us', 
+    component: AboutUs 
   },
   {
     path: 'contact',
---
+++ src/app/pages/about-us/about-us.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-us.html',
+  styleUrl: './about-us.css',
+})
+export class AboutUs {}
